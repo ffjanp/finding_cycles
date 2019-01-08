b2e88d27460ee695b68c7e674207b6a283dccdac
JanPel <jan.pel@futurefacts.nl>
2019-01-08 21:34:39 +0100

    added Monte Carlo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+extern crate rand;
+extern crate rand_xoshiro;
 use std::fs::File;
 use std::io::{ BufReader,Error, BufWriter,stdout,stdin};
 use std::collections::HashMap;
+use std::iter::Sum;
 use std::env;
 use std::thread;
-
+use std::cmp;
+use rand::Rng;
+use rand::SeedableRng;
+use rand_xoshiro::Xoroshiro128StarStar;
+use rand::distributions::{Distribution, Standard};
 
 fn read_number<R: std::io::BufRead>(io:&mut R,m:usize) -> Result<Vec<usize>,Error> {
     let mut v = vec![]; for _ in 0..m {
@@ -16,13 +23,18 @@ fn read_number<R: std::io::BufRead>(io:&mut R,m:usize) -> Result<Vec<usize>,Erro
     Ok(v)
 }
 
-fn read_edges<R: std::io::BufRead>(io:&mut R,m:usize) -> Result<Vec<Vec<usize>>,Error> {
+fn read_edges<R: std::io::BufRead>(io:&mut R,m:usize) -> Result<Vec<(Vec<usize>,Vec<f32>)>,Error> {
     let mut v = vec![];
     for _ in 0..m {
         let mut line = String::new();
         io.read_line(&mut line)?;
-        let edge: Vec<usize> = line.split(' ').map(|x| x.trim().parse::<usize>().unwrap()).collect();
-        v.push(edge);
+        let splitted: Vec<&str> = line.split(';').collect();
+        let edge: Vec<usize> = splitted[0].split(' ').map(|x| x.trim().parse::<usize>().unwrap()).collect();
+        let mut weights: Vec<f32> = vec![];
+        if splitted.len() > 1 {
+            weights = splitted[1].split(' ').map(|x| x.trim().parse::<f32>().unwrap()).collect();
+        }
+        v.push((edge,weights));
     }
     Ok(v)
 }
@@ -35,7 +47,7 @@ fn remove_edges(graph:&mut HashMap<usize,Vec<usize>>,node : usize) {
     }
     graph.remove(&node);    
 } 
-fn find_cycles<W: std::io::Write>(graph:&mut HashMap<usize,Vec<usize>>,startnode : usize,n:usize,file:&mut W) -> Vec<Vec<usize>>  {
+fn find_cycles (graph:&HashMap<usize,Vec<usize>>,startnode : usize,n:usize) -> Vec<Vec<usize>>  {
     let mut path = vec![startnode];
     let mut stack = vec![(startnode,graph[&startnode].to_vec())];
     let mut cycles = vec![];
@@ -72,28 +84,62 @@ fn make_subgraph(graph: &HashMap<usize,Vec<usize>>, cycle: &Vec<usize>) -> HashM
     }
     subgraph
 }
+fn random_remove_edges<T: Rng>(graph :&HashMap<usize,Vec<usize>>, weights: &HashMap<(usize,usize),f32>, random:&mut T) -> HashMap<usize,Vec<usize>> {
+    let mut new_graph: HashMap<usize,Vec<usize>> = HashMap::new();
+    for (&node,edges) in graph.iter() {
+        let mut new_edges :Vec<usize>= vec![];
+        for &edge in edges.iter() {
+            let value: f32 = random.sample(Standard);
+            if value < weights[&(node,edge)] {
+                new_edges.push(edge);
+            }
+        new_graph.insert(node,new_edges.to_vec());
+        }
+    }
+    new_graph
+}
+fn monte_carlo<T: Rng>(graph :&HashMap<usize,Vec<usize>>,weights: &HashMap<(usize,usize),f32>,mut random:&mut T,iterations: usize) -> f32 {
+    let mut results : Vec<usize> = vec![];
+    for i in 0..iterations {
+        let new_graph = random_remove_edges(&graph , &weights, &mut random);
+//        println!("----------------- here is removed graph");
+//        println!("{:?}",new_graph);
+        let mut length: usize = new_graph.len();
+        let mut max:usize= 0;
+        for &node in new_graph.keys() {
+            let new_max:usize = find_cycles(&new_graph,node,length).iter().map(|x| x.len()).max().unwrap_or(0);
+            max = cmp::max(max,new_max);
+        }
+        results.push(max); 
+    }
+    let result:usize = results.iter().sum();
+    (result as f32) / ((graph.len() * iterations) as f32)
+}
 
 
 
-
-fn cycle_worker(this_worker: usize,total_workers:usize,nodes:Vec<usize>,edges: Vec<Vec<usize>>) {
+fn cycle_worker(this_worker: usize,total_workers:usize,nodes:Vec<usize>,edges: Vec<(Vec<usize>,Vec<f32>)>) {
      // READING THE COMMAND LINE ARGUMENTS 
     let args: Vec<String> = env::args().collect();
     let length :usize = args[1].parse().unwrap();
 
    
-    //MAKING THE GRAPH, PER NODE THE EDGES OUT ARE STORED in  a sorted list
+    //MAKING THE GRAPHS, PER NODE THE EDGES OUT ARE STORED in  a sorted list
     let mut graph: HashMap<usize,Vec<usize>> = HashMap::new();
+    let mut weights: HashMap<(usize,usize),f32> = HashMap::new();
     for &node in &nodes {
         graph.insert(node,vec![]);
     }
     for edge in edges {
-        graph.entry(edge[0]).or_insert(vec![]).push(edge[1]);
+        graph.entry(edge.0[0]).or_insert(vec![]).push(edge.0[1]);
+        weights.insert((edge.0[0],edge.0[1]),edge.1[0]);
     }
     for (_,likes) in &mut graph {
         likes.sort_unstable();
     }
-
+    // Random Number generation
+    let mut rng = Xoroshiro128StarStar::seed_from_u64(0); 
+ 
     // HERE THE NODES AND EDGES ARE REMOVED WHICH ARE SKIPPED
     //for &n in &nodes[0..begin] {
     //    remove_edges(&mut graph,n);
@@ -104,8 +150,12 @@ fn cycle_worker(this_worker: usize,total_workers:usize,nodes:Vec<usize>,edges: V
     let mut i = 1;
     for &n in &nodes {
         if i % total_workers == this_worker { 
-            let subgraphs:Vec<HashMap<usize,Vec<usize>>> = find_cycles(&mut graph,n,length,&mut file).iter().map(|cycle| make_subgraph(&graph,cycle)).collect();
-            println!("{:?}",cycles);
+            let subgraphs:Vec<HashMap<usize,Vec<usize>>> = find_cycles(&graph,n,length).iter().map(|cycle| make_subgraph(&graph,cycle)).collect();
+            //println!("{:?}",subgraphs);
+            for subgraph in subgraphs.iter() {
+                let nodes : Vec<usize> = subgraph.keys().map(|&x| x).collect();
+                println!("{:?} ; {}",nodes,monte_carlo(&subgraph,&weights,&mut rng,1000));
+            }
         }
         i = i + 1;
         remove_edges(&mut graph, n);
